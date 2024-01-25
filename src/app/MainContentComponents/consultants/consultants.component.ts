18b4f1f272261e1c6b675f26c8cdbb321262b33a
Gennie Newell <57022209+gennienewell@users.noreply.github.com>
2024-01-25 17:54:46 -0500

    Update consultants.component.ts

--- src/app/MainContentComponents/consultants/consultants.component.ts
+++ src/app/MainContentComponents/consultants/consultants.component.ts
@@ -7,9 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ConsultantsComponent {
 
-  // Retrieve All Users from Database and set to a list or find a way to store them
-
-  // Make the consultant component clickable
-
-  // Add function to search through users 
+ 
 }
